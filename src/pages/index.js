e036a5fae0e94a8ccaf4e576a9a6f3a139369992
furyhawk <furyx@hotmail.com>
2021-04-04 21:52:01 +0800

    chapter 13 124

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Layout from '../components/Layout';
+
 // import our routes
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
 
 // define our routes
-const Pages = props => {
+const Pages = () => {
   return (
     <Router>
+      <Layout>
       <Route exact path="/" component={Home} />
       <Route path="/mynotes" component={MyNotes} />
       <Route path="/favorites" component={Favorites} />
+      </Layout>
     </Router>
   );
 };
