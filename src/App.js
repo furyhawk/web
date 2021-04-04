1703771ff06a3ec37af3db5aec0677240f7a9c1d
furyhawk <furyx@hotmail.com>
2021-04-04 23:28:39 +0800

    chapter 13 final

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 // This is the main entry point of our application
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import GlobalStyle from '/components/GlobalStyle';
 import Pages from '/pages';
 
 const App = () => {
     return (
         <div>
+            <GlobalStyle />
             <Pages />
         </div>
     );
